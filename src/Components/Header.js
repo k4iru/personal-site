c6b0aa3c1e0e17875c665e5f71a82f74c9872965
Kyle Cheung <kcheu4@uwo.ca>
2019-01-29 03:35:17 -0500

    edited

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -25,8 +25,7 @@ class Header extends Component {
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+            <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
@@ -34,8 +33,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h1 className="responsive-headline">Create Your Reality</h1>
+            <h3><span>{occupation}</span>.</h3>
             <hr />
             <ul className="social">
                {networks}
